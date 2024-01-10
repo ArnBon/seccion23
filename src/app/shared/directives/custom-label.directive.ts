358f8c6ee9d5ec7273692c75c9d6cb6f38e74c5b
ArnBon <bonillaarnaldo@gmail.com>
2024-01-10 19:41:03 -0400

    clase 320 resolucion tarea

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -53,7 +53,19 @@ export class CustomLabelDirective implements OnInit {
       return;
     }
 
- 
+    //tarea clase 320
+    if ( errors.includes('minlength') )  {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+
+      this.htmlElement.nativeElement.innerText = `Mínimo ${current}/${ min } caracteres.`;
+      return;
+    }
+
+    if ( errors.includes('email') )  {
+      this.htmlElement.nativeElement.innerText = 'No tiene formato de correo.';
+      return;
+    }
 
 
 
